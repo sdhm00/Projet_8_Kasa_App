91f559637e54db4509d80f3b8f40bfb9f897775d
Pauline Hesse <pipelettelive@gmail.com>
2024-02-09 11:58:59 +0100

    modif 9/2/2023

--- kasa-app/src/components/Logement.jsx
+++ kasa-app/src/components/Logement.jsx
@@ -10,6 +10,48 @@ function Logement() {
           <p className="carousel-txt">1/4</p>
           <i className="fa-solid fa-chevron-right" id="arrow-right"></i>
         </div>
+        <div className="description-block">
+          <div className="logement-description">
+            <div className="logement-title">
+              <h1>Cozy loft on the Canal Saint-Martin</h1>
+              <p>Paris, Île-de-France</p>
+            </div>
+            <div className="logement-host">
+              <p>Alexandre <br/> Dumas</p>
+              <div className="host-picture"></div>
+            </div>
+          </div>
+          <div className="logement-rating">
+            <div className="logement-tag">
+              <p>Cozy</p>
+              <p>Canal</p>
+              <p>Paris</p>
+            </div>
+            <div className="logement-stars">
+              <i class="fa-solid fa-star active-stars"></i>
+              <i class="fa-solid fa-star active-stars"></i>
+              <i class="fa-solid fa-star active-stars"></i>
+              <i class="fa-solid fa-star inactive-stars"></i>
+              <i class="fa-solid fa-star inactive-stars"></i>
+            </div>
+          </div>
+          <div className="description-text">
+            <div className="block-text">
+              <div className="description-buttons">
+                <h2>Description</h2>
+                <i className="fa-solid fa-chevron-up" id="about-vector"></i>
+              </div>
+              <p>Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied). </p>
+            </div>
+            <div className="block-text">
+              <div className="description-buttons">
+                <h2>Équipements</h2>
+                <i className="fa-solid fa-chevron-up" id="about-vector"></i>
+              </div>
+              <p>Climatisation <br />Wi-Fi <br />Cuisine <br />Espace de travail <br />Fer à repasser <br />Sèche-cheveux <br />Cintres</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
