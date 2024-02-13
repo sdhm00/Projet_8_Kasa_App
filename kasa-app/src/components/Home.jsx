609c9baf63432686a1fb326ed83c81e79bb36e55
Pauline Hesse <pipelettelive@gmail.com>
2024-02-13 13:45:33 +0100

    modif sass 13/2/2023

--- kasa-app/src/components/Home.jsx
+++ kasa-app/src/components/Home.jsx
@@ -1,4 +1,5 @@
 import img1 from "../assets/img1.png"
+import img3 from "../assets/img3.png"
 import "../styles/styles.css"
 import { Link } from "react-router-dom"
 
@@ -13,35 +14,53 @@ function Home() {
         <div className="logement-background">
           <div className="logement">
             <Link to="/logement">
-              <div className="cards-background">
-                <p>Titre de la <br/>location</p>
+              <div className="log-block">
+                <img src={img3} alt="img logement" className="img-log"/>
+                <div className="cards-background">
+                  <p>Titre de la <br/>location</p>
+                </div>
               </div>
             </Link>
             <Link to="/logement">
-              <div className="cards-background">
-                <p>Titre de la <br/>location</p>
+              <div className="log-block">
+                <img src={img3} alt="img logement" className="img-log"/>
+                <div className="cards-background">
+                  <p>Titre de la <br/>location</p>
+                </div>
               </div>
             </Link>
             <Link to="/logement">
-              <div className="cards-background">
-                <p>Titre de la <br/>location</p>
+              <div className="log-block">
+                <img src={img3} alt="img logement" className="img-log"/>
+                <div className="cards-background">
+                  <p>Titre de la <br/>location</p>
+                </div>
               </div>
             </Link>
           </div>
           <div className="logement">
             <Link to="/logement">
-              <div className="cards-background">
-                <p>Titre de la <br/>location</p>
+              <div className="log-block">
+                <img src={img3} alt="img logement" className="img-log"/>
+                <div className="cards-background">
+                  <p>Titre de la <br/>location</p>
+                </div>
               </div>
             </Link>
             <Link to="/logement">
-              <div className="cards-background">
-                <p>Titre de la <br/>location</p>
+              <div className="log-block">
+                <img src={img3} alt="img logement" className="img-log"/>
+                <div className="cards-background">
+                  <p>Titre de la <br/>location</p>
+                </div>
               </div>
             </Link>
             <Link to="/logement">
-              <div className="cards-background">
-                <p>Titre de la <br/>location</p>
+              <div className="log-block">
+                <img src={img3} alt="img logement" className="img-log"/>
+                <div className="cards-background">
+                  <p>Titre de la <br/>location</p>
+                </div>
               </div>
             </Link>
           </div>
