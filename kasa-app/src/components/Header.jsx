7950178ab582cb207c7c54aeccd9fab925ce2bd7
Pauline Hesse <pipelettelive@gmail.com>
2024-02-27 10:55:59 +0100

    modif link header 27/2/2023

--- kasa-app/src/components/Header.jsx
+++ kasa-app/src/components/Header.jsx
@@ -1,15 +1,23 @@
 import logo from "../assets/logo.png"
 import "../styles/styles.css"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
+import { useState, useEffect } from 'react'
 
 function Header() {
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState("/");
+
+  useEffect(() => {
+    setActiveLink(location.pathname);
+  }, [location]);
+
   return (
     <header className="header">
     <nav className="nav-block">
       <img src={logo} alt="Kasa-Header-logo" />
       <div className="navigation">
-        <Link to="/">Acceuil</Link>
-        <Link to="/about">A Propos</Link>
+        <Link to="/" className={activeLink === "/" ? "active" : ""}>Acceuil</Link>
+        <Link to="/about" className={activeLink === "/about" ? "active" : ""}>A Propos</Link>
       </div>
     </nav>
   </header>
