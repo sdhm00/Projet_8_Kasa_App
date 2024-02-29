b0b189f6be01e57a5a8f715bdfb62340f2ac53a5
Pauline Hesse <pipelettelive@gmail.com>
2024-02-29 14:45:16 +0100

    modif dropdown about 29/2/2023

--- kasa-app/src/components/Dropdown/Dropdown.jsx
+++ kasa-app/src/components/Dropdown/Dropdown.jsx
@@ -1,16 +1,46 @@
 import "../../styles/styles.css"
+import PropTypes from 'prop-types'
+import Description from "../Logement-Page/Description";
+import Equipments from "../Logement-Page/Equipments";
 import { useState, useEffect } from 'react';
 
-function Dropdown() {
-    const [rotated, setRotated] = useState(false);
+function Dropdown({logement}) {
+    const [Rotated, setRotated] = useState(false);
 
     const handleRotate = () => {
-      setRotated(!rotated);
+        setRotated(prevRotated => !prevRotated);
     };
 
     return (
-        <i className={`fa-solid fa-chevron-up ${rotated ? 'rotated' : ''}`} id="about-vector" onClick={handleRotate}></i>
-    );
+        <div className="description-text">
+            <div className="block-text">
+                <div className="description-buttons">
+                    <h2>Description</h2>
+                    <i className={`fa-solid fa-chevron-up ${descriptionRotated ? 'rotated' : ''}`} id="about-vector" onClick={handleDescriptionRotate}></i>
+                </div>
+                <div>
+                    {descriptionRotated && (
+                        <Description description={logement.description} />
+                    )}
+                </div>
+            </div>
+            <div className="block-text">
+                <div className="description-buttons">
+                    <h2>Equipements</h2>
+                    <i className={`fa-solid fa-chevron-up ${equipmentsRotated ? 'rotated' : ''}`} id="about-vector" onClick={handleEquipmentsRotate}></i>
+                </div>
+                <div>
+                    {equipmentsRotated && (
+                        <Equipments equipments={logement.equipments} />
+                    )}
+                </div>
+            </div>
+        </div>
+    )
 }
-  
-export default Dropdown;
+
+Dropdown.propTypes = {
+    logement: PropTypes.object.isRequired,
+}
+ 
+export default Dropdown
--- kasa-app/src/pages/About/About.jsx
+++ kasa-app/src/pages/About/About.jsx
@@ -1,8 +1,17 @@
 import img2 from "../../assets/img2.png"
-import Description from "../../components/Logement-Page/Description";
 import "../../styles/styles.css"
+import { useState, useEffect } from 'react';
 
 function About() {
+  const [rotations, setRotations] = useState({});
+
+  const handleRotation = (id) => {
+    setRotations(prevRotations => ({
+      ...prevRotations,
+      [id]: !prevRotations[id]
+    }));
+  };
+
   return (
     <div className="about-block">
       <div className="img-block">
@@ -13,38 +22,46 @@ function About() {
         <div className="about-infos">
           <div className="about-buttons">
             <h1>Flabilité</h1>
-            <Description />
+            <i className={`fa-solid fa-chevron-up ${rotations['flabilite'] ? 'rotated' : ''}`} id="about-vector" onClick={() => handleRotation('flabilite')}></i>
           </div>
-          <div className="about-txt">
-            <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.</p>
-          </div>
+          {rotations['flabilite'] && (
+            <div className="about-txt">
+              <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.</p>
+            </div>
+          )}
         </div>
         <div className="about-infos">
           <div className="about-buttons">
             <h1>Respect</h1>
-            <Description />
+            <i className={`fa-solid fa-chevron-up ${rotations['respect'] ? 'rotated' : ''}`} id="about-vector" onClick={() => handleRotation('respect')}></i>
           </div>
-          <div className="about-txt">
-            <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
-          </div>
+          {rotations['respect'] && (
+            <div className="about-txt">
+              <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+            </div>
+          )}
         </div>
         <div className="about-infos">
           <div className="about-buttons">
             <h1>Service</h1>
-            <Description />
+            <i className={`fa-solid fa-chevron-up ${rotations['service'] ? 'rotated' : ''}`} id="about-vector" onClick={() => handleRotation('service')}></i>
           </div>
-          <div className="about-txt">
-            <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
-          </div>
+          {rotations['service'] && (
+            <div className="about-txt">
+              <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+            </div>
+          )}
         </div>
         <div className="about-infos">
           <div className="about-buttons">
             <h1>Sécurité</h1>
-            <Description />
+            <i className={`fa-solid fa-chevron-up ${rotations['securite'] ? 'rotated' : ''}`} id="about-vector" onClick={() => handleRotation('securite')}></i>
           </div>
-          <div className="about-txt">
-            <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
-          </div>
+          {rotations['securite'] && (
+            <div className="about-txt">
+              <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
