f2b171a97957ce2d650563d9fbed749b8d5d9a6f
Pauline Hesse <pipelettelive@gmail.com>
2024-03-06 10:32:36 +0100

    modif 6/3/2023

--- kasa-app/src/components/Carrousel/Carrousel.jsx
+++ kasa-app/src/components/Carrousel/Carrousel.jsx
@@ -7,17 +7,28 @@ function Carrousel({pictures}) {
     const nextSlide = () => {
         setSlide(slideIndex === pictures.length - 1 ? 0 : slideIndex + 1)
     }
-
+    
     const prevSlide = () => {
         setSlide(slideIndex === 0 ? pictures.length - 1 : slideIndex - 1)
     }
 
+    const renderArrowsAndText = () => {
+        if (pictures.length === 1) {
+            return null;
+        }
+        return (
+            <>
+                <i className="fa-solid fa-chevron-left" id="arrow-left" onClick={prevSlide}></i>
+                <span className="carousel-txt">{slideIndex + 1}/{pictures.length}</span>
+                <i className="fa-solid fa-chevron-right" id="arrow-right" onClick={nextSlide}></i>
+            </>
+        );
+    }
+
     return (
         <div className="logement-carousel">
-            <i className="fa-solid fa-chevron-left" id="arrow-left" onClick={prevSlide}></i>
-                <img src={pictures[slideIndex]} alt="Carrousel img" className="logement-img"  />
-            <span className="carousel-txt">{slideIndex + 1}/{pictures.length}</span>
-            <i className="fa-solid fa-chevron-right" id="arrow-right" onClick={nextSlide}></i>
+            {renderArrowsAndText()}
+            <img src={pictures[slideIndex]} alt="Carrousel img" className="logement-img"  />
         </div>
     )
 }
