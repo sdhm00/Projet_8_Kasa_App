8e7a8d5add4f0c9e86d0739ae076e445a0e637fb
Pauline Hesse <pipelettelive@gmail.com>
2024-03-12 11:39:23 +0100

    modif 12/3/2023

--- kasa-app/src/components/Cards/Cards.jsx
+++ kasa-app/src/components/Cards/Cards.jsx
@@ -4,12 +4,10 @@ import PropTypes from 'prop-types'
 
 function Card({ cover, title, id }) {
     return (
-        <Link to={`/logement/${id}`}>
-            <div className="cards-background">
-                <img src={cover} alt="Logements cover" />
-                <div className="card-span">
-                    <p>{title}</p>
-                </div>
+        <Link to={`/logement/${id}`} className="cards-background">
+            <img src={cover} alt="Logements cover" />
+            <div className="card-span">
+                <p>{title}</p>
             </div>
         </Link>
     )
